93ab986cc7006497d1882a9e8a83319cdd2dbe88
Christophersipus <49676071+Christophersipus@users.noreply.github.com>
2021-01-07 10:24:57 +0700

    Update hospital.php

--- hospital.php
+++ hospital.php
@@ -5,7 +5,18 @@ $page = "Hospital";
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+<style>
+    table{
+        margin: 5px;
+    }
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th{
+        text-transform: uppercase;
+    }
+</style>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -20,12 +31,34 @@ $page = "Hospital";
     //Ambil Data RS
     $url_RS_Rujukan="https://dekontaminasi.com/api/id/covid19/hospitals";
     $res=ambil_url($url_RS_Rujukan);
-    $nama=$res->name;
-    $alamat=$res->address;
-    $daerah=$res->region;
-    $tlp=$res->phone;
-    $provinsi=$res->province;
+
+    function build_table($res){
+        // start table
+        $html = '<table>';
+        // header row
+        $html .= '<tr>';
+        foreach($res[0] as $key=>$value){
+                $html .= '<th>' . htmlspecialchars($key) . '</th>';
+            }
+        $html .= '</tr>';
+    
+        // data rows
+        foreach( $res as $key=>$value){
+            $html .= '<tr>';
+            foreach($value as $key2=>$value2){
+                $html .= '<td>' . htmlspecialchars($value2) . '</td>';
+            }
+            $html .= '</tr>';
+        }
+    
+        // finish table and return it
+    
+        $html .= '</table>';
+        return $html;
+    }
+    echo build_table($res);
 
 ?>
+
 </body>
 </html>
