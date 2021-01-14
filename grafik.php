2fef24ce6c7f76a558b581006e6fb1c23a683f65
Dini Puspita <clinophile13@gmail.com>
2021-01-14 08:33:31 +0700

    Update grafik.php

--- grafik.php
+++ grafik.php
@@ -1,6 +1,9 @@
 <?php
 include 'corona.php';
 $page = "Grafik";
+
+$url_global="https://api.kawalcorona.com/";
+    $result2=ambil_url($url_global);
 ?>
 <!doctype html>
 <html lang="en">
@@ -30,21 +33,29 @@ $page = "Grafik";
 
     $prov_pos = array() ;    
     for ($x = 0; $x <= 10; $x++) {
-        array_push($prov_pos, array("label"=> $data_prov[$x]->attributes->Provinsi, "y"=> $data_prov[$x]->attributes->Kasus_Posi)) ;
+        array_push($prov_pos, array("label"=> $data_prov[$x]->attributes->Provinsi, "y"=> ($data_prov[$x]->attributes->Kasus_Posi - $data_prov[$x]->attributes->Kasus_Meni - $data_prov[$x]->attributes->Kasus_Semb))) ;
+    }
+
+//    $dataPerbandingan = array(array("y"=> 268583016, "label"=> "Jumlah Total Penduduk Indonesia"),
+// 	array("y"=> 790000, "label"=> "Jumlah Kasus Positif COVID-19")
+//    )
+
+	$global_pos = array() ;    
+    for ($x = 0; $x <= 10; $x++) {
+        array_push($global_pos, array("label"=> $result2[$x]->attributes->Country_Region, "y"=> $result2[$x]->attributes->Confirmed)) ;
     }
 
 ?>
 
 
 <script>
-window.onload = function () {
- 
+window.onload = function() {
 var chart = new CanvasJS.Chart("chartContainer", {
 	animationEnabled: true,
 	exportEnabled: true,
 	theme: "light1", // "light1", "light2", "dark1", "dark2"
 	title:{
-		text: "10 PROVINSI DENGAN KASUS COVID-19 TERBANYAK"
+		text: "10 PROVINSI DI INDONESIA DENGAN KASUS COVID-19 TERBANYAK"
 	},
 	axisX:{
 		reversed: true
@@ -65,17 +76,58 @@ var chart = new CanvasJS.Chart("chartContainer", {
 		dataPoints: <?php echo json_encode($prov_meni, JSON_NUMERIC_CHECK); ?>
     },{
 		type: "stackedBar",
-		name: "Positif",
+		name: "Aktif",
 		dataPoints: <?php echo json_encode($prov_pos, JSON_NUMERIC_CHECK); ?>
 	}
 ]
 });
 chart.render();
  
-}
+//  var chart2 = new CanvasJS.Chart("chartContainer2", {
+// 	animationEnabled: true,
+// 	theme: "light2",
+// 	title:{
+// 		text: "Perbandingan Total Penduduk dan Jumlah Kasus Positif COVID-19 di Indonesia"
+// 	},
+// 	axisY: {
+// 		title: "Jumlah"
+// 	},
+// 	data: [{
+// 		type: "column",
+		// dataPoints:  <--?php echo json_encode($dataPerbandingan, JSON_NUMERIC_CHECK); ?>
+// 	}]
+// });
+// chart2.render();
+ 
+var chart2 = new CanvasJS.Chart("chartContainer2", {
+	animationEnabled: true,
+	title:{
+		text: "10 NEGARA DENGAN KASUS COVID-19 TERBANYAK"
+	},
+	axisX:{
+		reversed: true
+	},
+	axisY: {
+		title: "Confirmed Case",
+		includeZero: true,
+	},
+	data: [{
+		type: "bar",
+//		yValueFormatString: "$#,##0K",
+		indexLabel: "{y}",
+		indexLabelPlacement: "inside",
+		indexLabelFontWeight: "bolder",
+		indexLabelFontColor: "white",
+		dataPoints: <?php echo json_encode($global_pos, JSON_NUMERIC_CHECK); ?>
+	}]
+});
+chart2.render(); 
+} 
 </script>
 
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
+<div id="chartContainer" style="height: 370px; width: 100%;"></div> <br> <br>
+<div id="chartContainer2" style="height: 370px; width: 100%;"></div> <br>
+
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 </body>
 </html>               
