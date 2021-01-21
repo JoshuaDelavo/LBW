d7d0d8e3442ad066d89c88b296d3bf6951825ac1
ChristopherLunadi <55026909+exaponz@users.noreply.github.com>
2021-01-21 10:33:09 +0700

    Update Info.php

--- info.php
+++ info.php
@@ -16,5 +16,244 @@
         $page = "Info";
         include 'header.php';
     ?>
+
+<div class="jumbotron text-center">
+           <h1>Apa yang Harus Diketahui</h1>
+</div>  
+
+        <div class="container mb-5">
+            <div class="card text-white bg-success">
+                    <div class="card-body">
+                        <div class="row mb-4 ml-3 mr-3 ">
+                            <h2 class="card-title">Apa Itu COVID-19?</h2>
+                                <div class="card-text">
+                                <p>
+                                COVID-19 adalah penyakit yang disebabkan oleh Novel Coronavirus (2019-nCoV), 
+                                jenis baru coronavirus pada manusia menyebabkan penyakit mulai dari flu biasa 
+                                hingga penyakit yang serius seperti Middle East Respiratory Syndrome (MERS)
+                                dan Sindrom Pernapasan Akut Berat/ Severe Acute Respiratory Syndrome (SARS).
+                                WHO mengumumkan nama penyakit yang disebabkan 2019-nCov, yaitu Coronavirus Disease (COVID-19).
+                                </p>
+                                </div>
+                            <h2 class="card-title">Gejala</h2>
+                                <div class="card-text">
+                                <p>Gejala umum berupa demam ≥ 38°C, batuk kering, dan sesak napas. 
+                                Jika ada orang yang dalam 14 hari sebelum muncul gejala tersebut pernah melakukan perjalanan ke negara 
+                                terjangkit, atau pernah merawat/kontak erat dengan penderita COVID-19, maka terhadap orang tersebut akan 
+                                dilakukan pemeriksaan laboratorium lebih lanjut untuk memastikan diagnosisnya. 
+                                </p>
+                                </div>
+                            <h2 class="card-title">Penularan</h2>
+                                <div class="card-text">
+                                <p>
+                                Seseorang dapat terinfeksi dari penderita COVID-19. Penyakit ini dapat menyebar melalui tetesan kecil (droplet) 
+                                dari hidung atau mulut pada saat batuk atau bersin. Droplet tersebut kemudian jatuh pada benda di sekitarnya. 
+                                Kemudian jika ada orang lain menyentuh benda yang sudah terkontaminasi dengan droplet tersebut, lalu orang itu menyentuh mata, 
+                                hidung atau mulut, maka orang itu dapat terinfeksi COVID-19. 
+                                Seseorang juga bisa terinfeksi COVID-19 ketika tidak sengaja menghirup droplet dari penderita. 
+                                Inilah sebabnya mengapa kita penting untuk menjaga jarak hingga kurang lebih satu meter dari orang yang sakit. 
+                                </p>
+                                </div>
+                            <h2 class="card-title">Pencegahan</h2>
+                                <div class="card-text">
+                                <p>Lindungi diri Anda dan orang lain di sekitar Anda dengan mengetahui fakta-fakta terkait virus ini dan mengambil langkah pencegahan yang sesuai. 
+                                Ikuti saran yang diberikan oleh otoritas kesehatan setempat.
+                                Beberapa cara untuk mencegah penyebaran COVID-19: </p>
+                                <ul>
+                                <li> Cuci tangan Anda secara rutin. Gunakan sabun dan air, atau cairan pembersih tangan berbahan alkohol.</li>
+                                <li> Selalu jaga jarak aman dengan orang yang batuk atau bersin.</li>
+                                <li> Kenakan masker jika pembatasan fisik tidak dimungkinkan.</li>
+                                <li> Jangan sentuh mata, hidung, atau mulut Anda.</li>
+                                <li> Saat batuk atau bersin, tutup mulut dan hidung Anda dengan lengan atau tisu.</li>
+                                <li> Jangan keluar rumah jika merasa tidak enak badan.</li>
+                                <li> Jika demam, batuk, atau kesulitan bernapas, segera cari bantuan medis.</li>
+                                </ul>
+                                <p>
+                                Masker dapat membantu mencegah penyebaran virus dari orang yang mengenakannya kepada orang lain. 
+                                Mengenakan masker saja tidak cukup untuk melindungi diri dari COVID-19, sehingga harus dikombinasikan dengan pembatasan fisik dan kebersihan tangan. 
+                                Ikuti saran yang diberikan oleh otoritas kesehatan setempat.
+                                </p>
+                                
+                                </div>    
+                        </div>
+                </div>
+
+            </div>
+        </div>
+
+
+        <div class="jumbotron text-center">
+           <h1>Sejarah COVID-19</h1>
+        </div>  
+         
+        
+        <div class="containertime mb-5">
+        <div class="timeline">
+        <div class="containers left">
+            <div class="content">
+                <h2>31 Desember 2019</h2>
+                <p>Kasus pneumonia yang terdeteksi di Wuhan, China pertama kali dilaporkan ke WHO.
+                Selama periode yang dilaporkan ini, virus belum diketahui.</p>
+            </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>1 Januari 2020</h2>
+            <p>Otoritas kesehatan China menutup Pasar Grosir Makanan laut Huanan setelah ditemukan
+                bahwa hewan liar yang dijual di sana mungkin merupakan sumber virus.
+            </p>
+            </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>7 Januari 2020</h2>
+            <p>Pihak berwenang China mengkonfirmasi bahwa telah mengidentifikasi virus tersebut sebagai virus corona
+                baru, yang awalnya oleh WHO disebut sebagai 2019-nCoV.
+            </p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>11 Januari 2020</h2>
+            <p>Komisi Kesehatan Wuhan mengumumkan kematian pertama yang disebabkan corona virus.</p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>13 Januari 2020</h2>
+            <p>Pemerintah Thailang melaporkan kasus infeksi yang disebabkan oleh virus corona.</p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>16 Januari 2020</h2>
+            <p>Pihan berwenang Jepang mengonfirmasi seorang pria Jepang yang bepergian ke Wuhan
+                terinfeksi virus corona.
+            </p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>17 Januari 2020</h2>
+            <p>Kasus kematian kedua dikonfirmasi oleh Pejabat Kesehatan China.</p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>20 Januari 2020</h2>
+            <p>China melaporkan 139 Kasus penyakit baru, termasuk kematian ketiga yang terjadi di wilayahnya.</p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>21 Januari 2020</h2>
+            <p>Para pejabat di negara bagian Washington mengonfirmasi kasus pertama di wilayah Amerika Serikat</p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>22 Januari 2020</h2>
+            <p>Pemerintah Wuhan sementara waktu menutup bandara dan stasiun kereta api
+                setelah jumlah korban tewas virus corona telah meningkat menjadi 17 orang.
+            </p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>26 Januari 2020</h2>
+            <p>Asosiasi Layanan Perjalanan China menangguhkan semua tur, termasuk tur internasional.</p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>30 Januari 2020</h2>
+            <p>Amerika Serikat melaporkan kasus penularan pertama dari orang ke orang atas virus corona Wuhan
+                WHO menetapkan bahwa wabah tersebut menjadi Darurat Kesehatan Internasional dan Kepedulian
+                Internasional(PHEIC).
+            </p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>2 Februari 2020</h2>
+            <p>Kasus kematian pertama di luar China karena virus corona Wuhan, tepatnya di Filipina</p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>4 Februari 2020</h2>
+            <p>Kementerian Kesehatan Jepang mengumumkan sepuluh orang di atas kapal pesiar Diamond Princess
+                yang berlabuh di Yokohama Bay dipastikan memiliki virus corona.
+            </p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>7 Februari 2020</h2>
+            <p>Li Wenliang, seorang dokter Wuhan yang menjadi target polisi karena berusaha menyembunyikan
+                alarm pada virus mirip SARS pada Desember, meninggal karena virus corona.
+            </p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>11 Februari 2020</h2>
+            <p>Penyakit akibat virus ini diberi nama oleh WHO sebagai Covid-19.</p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>14 Februari 2020</h2>
+            <p>Seorang turis China yang tes pemeriksaannya menunjukkan hasil positif terpapar
+                virus meninggal di Perancis, Mesir mengumumkan kasus pertama virus corona di wilayahnya.
+            </p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>29 Februari 2020</h2>
+            <p>Amerika mengumumkan kematian pertama akibat virus corona di Amerika Serikat.</p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>2 Maret 2020</h2>
+            <p>Pemerintah Indonesia secara resmi mengumumkan dua WNI positif terpapar
+                virus corona. Keduanya menjadi kasus pertama yang dilaporkan di Indonesia.
+            </p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>3 Maret 2020</h2>
+            <p>Pemerintah Iran mengumumkan bahwa 23 anggota parlemen Iran dinyatakan positif terpapar virus.</p>
+        </div>
+        </div>
+        <div class="containers left">
+            <div class="content">
+            <h2>8 Maret 2020</h2>
+            <p>Perdana menteri Italia Giuseppe Conte menandatangani keputusan yang menetapkan
+                pembatasan perjalanan di seluruh wilayah Lombardy dan 14 provinsi lainnya.
+            </p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>11 Maret 2020</h2>
+            <p>Indonesia mengumumkan kematian pertama akibat virus corona yaitu pasien kasus 25,
+                seorang WNA yang menjalani perawatan di RS Sanglah, Bali.
+            </p>
+        </div>
+        </div>
+        <div class="containers right">
+            <div class="content">
+            <h2>11 Maret 2020</h2>
+            <p>WHO menyatakan wabah corona virus sebagai pandemi saat Turki, Pantai, Gading, Honduras,
+                dan Bolivia mengonfirmasi kasus pertama di wilayahnya.
+            </p>
+        </div>
+        </div>
+        </div>
+
 </body>
 </html>
